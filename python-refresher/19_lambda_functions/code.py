9a5aad4609a93919bbb4031d31a6d911d96fa907
Naman <namangupta1399@gmail.com>
2020-11-27 10:56:55 +0530

    lamba_func, unpacking args

---
+++ python-refresher/19_lambda_functions/code.py
@@ -0,0 +1,13 @@
+# add = lambda x, y: x + y
+
+# print(add(5, 7))
+
+def double(x):
+    return x * 2
+
+sequence = [1, 3, 5, 9]
+# doubled = [(lambda x : x * 2)(x) for x in sequence]
+
+doubled = list(map(lambda x : x * 2, sequence))
+
+print(doubled)
